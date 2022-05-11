30caf64483bfb2caa68725523b691f614465063d
equne <equne@qq.com>
2022-05-11 15:25:36 +0800

    基本实现简单增删改查功能

--- src/com/equne/JDBC/jdbc_atmProject/TestMain.java
+++ src/com/equne/JDBC/jdbc_atmProject/TestMain.java
@@ -15,27 +15,60 @@ public class TestMain {
         String result = ta.login(aname, apassword);
         if(result.equals("登录成功")){
             System.out.println(aname + "，您好！欢迎进入蜜桃银行系统 ");
-            System.out.println("请输入操作选项序号");
-            System.out.println("1.查询 2.存款 3.取款 4.转账");
-            String option = input.nextLine();
-            switch (option){
-                case "1":
-                    float abalance = ta.check(aname);
-                    System.out.println("尊敬的"+ aname + "用户，您的可用余额为：" + abalance);
+            while(true){
+                System.out.println("-------------------------------------\n" + ">> 请输入操作选项序号");
+                System.out.println("1:查询  2:存款  3:取款  4:转账  \n5:开户  6:销户  886:退出");
+                String option = input.nextLine();
+                if (option.equals("886")){
+                    System.out.println("您已退出系统，再见！");
                     break;
-                case "2":
-                    System.out.println("功能完善中..");
-                    break;
-                case "3":
-                    System.out.println("功能完善中..");
-                    break;
-                case "4":
-                    System.out.println("功能完善中..");
-                    break;
-                default:
-                    System.out.println("请输入正确序号！");
-
+                }
+                switch (option){
+                    case "1":  // 1. 查询
+                        float abalance = ta.check(aname);
+                        System.out.println("尊敬的"+ aname + "用户，您的可用余额为：" + abalance);
+                        break;
+                    case "2":  // 2. 存款
+                        System.out.println("请输入存款额：");
+                        String depoistAmount = input.nextLine();
+                        ta.deposit(aname, Float.parseFloat(depoistAmount));
+                        System.out.println("您的可用余额为：" + ta.check(aname));
+                        break;
+                    case "3":  // 3. 取款
+                        System.out.println("请输入取款额：");
+                        String withdrawalAmount = input.nextLine();
+                        ta.withdraw(aname, Float.parseFloat(withdrawalAmount));
+                        System.out.println("您的可用余额为：" + ta.check(aname));
+                        break;
+                    case "4":  // 4. 转账
+                        System.out.println("请输入收款人信息：");
+                        String payee = input.nextLine();
+                        System.out.println("请输入转账金额：");
+                        String transferAmount = input.nextLine();
+                        ta.transfor(aname, payee, Float.parseFloat(transferAmount));
+                        System.out.println("您的可用余额为：" + ta.check(aname));
+                        break;
+                    case "5":  // 5. 开户
+                        System.out.println("请输入开户账户名：");
+                        String newAname = input.nextLine();
+                        System.out.println("请输入新账户密码：");
+                        String newApassword = input.nextLine();
+                        System.out.println("请输入存入金额：");
+                        String newAbalance = input.nextLine();
+                        ta.newBank(newAname, newApassword, Float.parseFloat(newAbalance));
+                        System.out.println("开户成功！新账户" + newAname + "当前可用余额为：" + ta.check(newAname));
+                        break;
+                    case "6":  // 6. 销户
+                        System.out.println("请输入销户账户名：");
+                        String cancelName = input.nextLine();
+                        ta.delete(cancelName);
+                        System.out.println("已成功删除该账户！");
+                        break;
+                    default:
+                        System.out.println("请输入正确序号！");
+                }
             }
+
         } else {
             System.out.println(result);
         }
--- src/com/equne/JDBC/jdbc_atmProject/test/TestAtm.java
+++ src/com/equne/JDBC/jdbc_atmProject/test/TestAtm.java
@@ -8,29 +8,152 @@ import java.sql.*;
 
 public class TestAtm {
 
-    // update：连接数据库、做数据库的写操作
-    // 封装存款/取款/修改密码的内部代码（代码冗余）、转账、修改密码
-    // 参数：Atm / 返回值：无 (int更新行数)
-    public void update(Atm atm){
-        String className = "com.mysql.cj.jdbc.Driver";
-        String url = "jdbc:mysql://localhost:3306";
-        String user = "root";
-        String password = "root";
+    // ————————————————————————————————————————————————————————————————————————
+    //    以下方法为纯粹的JDBC读写数据库操作，没有任何逻辑
+    // ————————————————————————————————————————————————————————————————————————
 
+    /* delete: 负责将某行记录从数据库删除
+     */
+    public void delete(String aname){
+        String className = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/atm";
+        String user = "root";
+        String password = "rootroot";
+        Connection conn = null;
+        Statement stat = null;
+        ResultSet rs = null;
+        String sql = "DELETE FROM atm WHERE aname ='" + aname +"'";
         try {
             Class.forName(className);
-        } catch (ClassNotFoundException e) {
+            conn = DriverManager.getConnection(url, user, password);
+            stat = conn.createStatement();
+            stat.executeUpdate(sql);
+        } catch (Exception e) {
             e.printStackTrace();
+        }finally {
+            try {
+                if (conn!=null){
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if(stat!=null){
+                    stat.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if(rs!=null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    // selectOne：查询一条记录
-    // 封装登录和查询的重复代码
-    // 参数：aname / 返回值：Object (domain) （返回值不能是ResultSet，因为程序最后会使用rs.close()将结果集关闭。可以将结果集中的数据取出来，将数据存在一个容器里返回。
+    /* insert: 负责将新一行记录写入数据库
+     */
+    public void insert(Atm atm){
+        String className = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/atm";
+        String user = "root";
+        String password = "rootroot";
+        String sql = "INSERT INTO atm VALUES('" + atm.getAname() +"','" + atm.getApassword() + "'," + atm.getAbalance() +")";
+        Connection conn = null;
+        Statement stat = null;
+        ResultSet rs = null;
+        try {
+            Class.forName(className);
+            conn = DriverManager.getConnection(url, user, password);
+            stat = conn.createStatement();
+            stat.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (conn!=null){
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if(stat!=null){
+                    stat.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if(rs!=null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    /* update：连接数据库、做数据库的写操作
+        封装：存款/取款/修改密码操作代码，通用的update操作代码
+        参数：Atm / 返回值：无 (int更新行数)
+     */
+    public void update(Atm atm){
+        String className = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/atm";
+        String user = "root";
+        String password = "rootroot";
+        Connection conn = null;
+        Statement stat = null;
+        ResultSet rs = null;
+        String sql = "UPDATE atm SET apassword = '" + atm.getApassword() + "', abalance = '" + atm.getAbalance() + "' WHERE aname ='" + atm.getAname() + "'";
+        try {
+            Class.forName(className);
+            conn = DriverManager.getConnection(url, user, password);
+            stat = conn.createStatement();
+            stat.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭流：放在finally内必需执行。
+            // 分成三个try/catch分开关闭，增强健壮性：当某个流出现异常无法关闭时，其他流也能正常关闭。
+            try {
+                if (conn!=null){ // 若为空时关闭会抛出空指针异，因此限定当流创建成功即非空时才关闭。
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if(stat!=null){
+                    stat.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if(rs!=null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /* selectOne：查询一条记录
+        封装：登录和查询的重复代码
+        参数：aname / 返回值：Object (domain) （返回值不能是ResultSet，因为程序最后会使用rs.close()将结果集关闭。可以将结果集中的数据取出来，将数据存在一个容器里返回。
+     */
     public Atm selectOne(String aname) {
         Atm atm = null;
         String className = "com.mysql.cj.jdbc.Driver";
-        String url = "jdbc:mysql://localhost:3306/test_atm_20220509";
+        String url = "jdbc:mysql://localhost:3306/atm";
         String user = "root";
         String password = "rootroot";
         String sql = "SELECT aname, apassword, abalance FROM atm WHERE aname = '" + aname + "'";
@@ -55,7 +178,7 @@ public class TestAtm {
             // 关闭流：放在finally内必需执行。
             // 分成三个try/catch分开关闭，增强健壮性：当某个流出现异常无法关闭时，其他流也能正常关闭。
             try {
-                if (conn!=null){ // 若为空即创建失败，则无需关闭，会直接抛出异常。因此限定当流创建成功即非空时才关闭。
+                if (conn!=null){ // 若为空时关闭会抛出空指针异，因此限定当流创建成功即非空时才关闭。
                     conn.close();
                 }
             } catch (SQLException e) {
@@ -79,7 +202,9 @@ public class TestAtm {
         return atm;
     }
 
-
+    // ————————————————————————————————————————————————————————————————————————
+    //    以下方法为处理业务逻辑：比较、判断、计算，看不到数据库的读写操作（JDBC）。
+    // ————————————————————————————————————————————————————————————————————————
 
     // login: 登录
     public String login(String aname, String apassword){
@@ -90,28 +215,77 @@ public class TestAtm {
         return "用户名或密码错误";
     }
 
-    // check: 查询余额
+    /* check: 查询余额
+     */
     public float check(String aname){
         // this.的写法省略了'Atm atm ='的过程。
         return this.selectOne(aname).getAbalance();
     }
 
-    // deposit: 存款
-    // updade，修改数据库内的信息
+    /* deposit: 存款
+        updade，修改数据库内的信息
+     */
     public void deposit(String aname, float depositAmount){
+        Atm atm = this.selectOne(aname);
+        if(depositAmount > 0){
+            atm.setAbalance(atm.getAbalance() + depositAmount) ;
+            this.update(atm);
+            System.out.println("存款成功。");
+        }else {
+            System.out.println("存款失败，存款额有误。");
+        }
 
     }
 
-    // withdraw：取款
-    // updade，修改数据库内的信息
+    /* withdraw：取款
+       updade，修改数据库内的信息
+     */
     public void withdraw(String aname, float withdrawalAmout){
-
+        Atm atm = this.selectOne(aname);
+        if(atm.getAbalance() >= withdrawalAmout){
+            atm.setAbalance(atm.getAbalance() - withdrawalAmout);
+            this.update(atm);
+            System.out.println("取款成功。");
+        } else {
+            System.out.println("取款失败，余额不足。");
+        }
     }
 
-    // 转账
+    /* transfer： 转账
+     */
+    public void transfor(String remitter, String payee, float transferAmount){
+        Atm atmRemitter = this.selectOne(remitter);
+        Atm atmPayee = this.selectOne(payee);
+        if (atmPayee.getAname().equals(payee)){
+            if(transferAmount <= atmRemitter.getAbalance() && transferAmount != 0){
+                atmRemitter.setAbalance(atmRemitter.getAbalance() - transferAmount);
+                atmPayee.setAbalance(atmPayee.getAbalance() + transferAmount);
+                this.update(atmRemitter);
+                this.update(atmPayee);
+                System.out.println("转账成功！");
+            } else {
+                System.out.println("转账失败，余额不足。");
+            }
+        } else {
+            System.out.println("转账失败，收款人信息有误。");
+        }
+
+    }
 
     // 开户
+    public void newBank(String aname, String apassword, float abalance){
+        this.insert(new Atm(aname, apassword, abalance));
+    }
 
     // 销户
-
+    public void cancelBank(String aname){
+        // 查找要销户的账户
+        Atm atm = this.selectOne(aname);
+        // 若有该账户则销户
+        if(atm!=null){
+            this.delete(aname);
+        } else {
+            System.out.println("销户失败，未查询到该账户");
+        }
+    }
 }
